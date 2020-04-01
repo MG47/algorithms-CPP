d94f5a6055f1bc79c663fa2d82602a7fb39e4f1b
Mihir Ganu <mihirganu@gmail.com>
2020-04-01 10:30:19 -0700

    CPP_Programming: Regorganize Class program
    Regorganize and add basic class programs.

---
+++ general/CPP_programming/classes-II.cpp
@@ -0,0 +1,209 @@
+/*
+* Program to study classes - PART II
+*/
+
+#include <cstdint>
+#include <cstdio>
+
+
+/*
+* PART II:
+* Copy constructor/Initialization
+* overloaded assignment operator
+* deep copying, copy elision
+* explicit, delete
+*/
+
+
+// =========================================
+// Buffer Class - default copy constructor
+// =========================================
+
+class Buffer {
+public:
+
+	typedef enum : uint32_t {
+		TYPE_INVALID,
+		TYPE_LINEAR,
+		TYPE_GRAPHIC,
+	} type_t;
+
+	Buffer() = default;
+	explicit Buffer(uint32_t len, uint32_t size, type_t type) :
+		m_len(len),
+		m_size(size),
+		m_type(type)
+		{}
+
+	Buffer &operator=(Buffer &buf)
+	{
+		m_len = buf.m_len;
+		m_size = buf.m_size;
+		m_type = buf.m_type;
+		return *this;
+	}
+
+	uint32_t getSize() { return m_size; };
+	uint32_t getLength() { return m_len; };
+	type_t getType() const { return m_type; };
+	void printBufInfo();
+
+private:
+	uint32_t m_len{0};
+	uint32_t m_size{0};
+	type_t m_type{TYPE_INVALID};
+};
+
+void Buffer::printBufInfo()
+{
+	printf("New buffer ==============\n");
+	printf("Buffer length = %u\n", this->getLength());
+	printf("Buffer size = %u\n", this->getSize());
+	printf("Buffer type = %s\n",
+		(this->getType() == TYPE_INVALID) ? "INVALID" :
+		this->getType() == TYPE_GRAPHIC ? "GRAPHIC" : "LINEAR");
+}
+
+// =========================================
+// CBuffer Class - default copy constructor
+// =========================================
+
+class CBuffer {
+public:
+
+	typedef enum : uint32_t {
+		TYPE_INVALID,
+		TYPE_LINEAR,
+		TYPE_GRAPHIC,
+	} type_t;
+
+	CBuffer() = default;
+	CBuffer(uint32_t len, uint32_t size, type_t type) :
+		m_len(len),
+		m_size(size),
+		m_type(type)
+		{}
+
+	// Copy constructor
+	CBuffer(CBuffer &cbuf) :
+		m_len(cbuf.m_len),
+		m_size(cbuf.m_size),
+		m_type(cbuf.m_type)
+		{
+			printf("CBuffer - Copy constructor\n");
+		}
+
+
+	uint32_t getSize() { return m_size; };
+	uint32_t getLength() { return m_len; };
+	type_t getType() const { return m_type; };
+	void printBufInfo();
+
+private:
+	Buffer &operator=(Buffer &buf) = delete;
+	uint32_t m_len{0};
+	uint32_t m_size{0};
+	type_t m_type{TYPE_INVALID};
+};
+
+void CBuffer::printBufInfo()
+{
+	printf("New buffer ==============\n");
+	printf("Buffer length = %u\n", this->getLength());
+	printf("Buffer size = %u\n", this->getSize());
+	printf("Buffer type = %s\n",
+		(this->getType() == TYPE_INVALID) ? "INVALID" :
+		this->getType() == TYPE_GRAPHIC ? "GRAPHIC" : "LINEAR");
+}
+
+// =========================================
+// GBuffer Class - private copy constructor
+// =========================================
+
+class GBuffer {
+public:
+
+	typedef enum : uint32_t {
+		TYPE_INVALID,
+		TYPE_LINEAR,
+		TYPE_GRAPHIC,
+	} type_t;
+
+	GBuffer() = delete;
+	explicit GBuffer(uint32_t len, uint32_t size, type_t type) :
+		m_len(len),
+		m_size(size),
+		m_type(type)
+		{}
+
+
+	uint32_t getSize() { return m_size; };
+	uint32_t getLength() { return m_len; };
+	type_t getType() const { return m_type; };
+	void printBufInfo();
+
+private:
+	// Copy constructor
+	GBuffer(GBuffer &gbuf);
+	GBuffer &operator=(GBuffer &buf) = delete;
+	uint32_t m_len{0};
+	uint32_t m_size{0};
+	type_t m_type{TYPE_INVALID};
+};
+
+void GBuffer::printBufInfo()
+{
+	printf("New buffer ==============\n");
+	printf("Buffer length = %u\n", this->getLength());
+	printf("Buffer size = %u\n", this->getSize());
+	printf("Buffer type = %s\n",
+		(this->getType() == TYPE_INVALID) ? "INVALID" :
+		this->getType() == TYPE_GRAPHIC ? "GRAPHIC" : "LINEAR");
+}
+
+// =========================================
+// Tests
+// =========================================
+
+
+int main()
+{
+	Buffer b1(5, 10, Buffer::TYPE_LINEAR);
+	b1.printBufInfo();
+
+	// Default copy constructor
+	Buffer b2(b1);
+	b2.printBufInfo();
+
+	// copy constructor
+	CBuffer c1(20, 20, CBuffer::TYPE_GRAPHIC);
+	CBuffer c2(c1);
+	c2.printBufInfo();
+
+	// cannot call private copy constructor
+	GBuffer g1(20, 40, GBuffer::TYPE_GRAPHIC);
+//	GBuffer g2(g1);
+
+	// copy elision
+	Buffer b3(Buffer{20, 50, Buffer::TYPE_GRAPHIC});
+	b3.printBufInfo();
+
+	// Default constructor is deleted, invalid call
+	// GBuffer g3;
+
+
+	// Copy assignment
+	Buffer b4 = b3;
+	b4.printBufInfo();
+
+	// Copy assignment - default assignment
+	CBuffer c3 = c2;
+	c3.printBufInfo();
+
+	// Copy assignment is now allowed
+	GBuffer g2(2, 4, GBuffer::TYPE_GRAPHIC);
+	//g2 = g1;
+
+	return 0;
+}
+
---
+++ general/CPP_programming/classes.cpp
@@ -0,0 +1,203 @@
+/*
+* Program to study classes - PART I
+*/
+
+#include <cstdint>
+#include <cstdio>
+
+
+/*
+* PART I:
+* Test Basic classes, access specifiers
+* constructor/destructors
+* const members, static members,
+* friend functions/classes,
+* anonymous objects, class nesting
+*/
+
+// =========================================
+// Buffer Class
+// =========================================
+
+class Buffer {
+public:
+
+	typedef enum : uint32_t {
+		TYPE_INVALID,
+		TYPE_LINEAR,
+		TYPE_GRAPHIC,
+	} type_t;
+
+	Buffer() = default;
+	Buffer(uint32_t len, uint32_t size, type_t Type);
+	Buffer(uint32_t len) : m_len(len) {}
+	~Buffer();
+
+	uint32_t getSize();
+	uint32_t getLength();
+	type_t getType() const;
+	void printBufInfo();
+	static uint32_t get_destruct_count();
+	friend void reset(Buffer *buf);
+	friend class BufferFiller;
+
+	// Nested class
+	class BufferPrinter {
+	public:
+		void print(Buffer &buf)
+		{
+			printf("New buffer ==============\n");
+			printf("Buffer length = %u\n", buf.getLength());
+			printf("Buffer size = %u\n", buf.getSize());
+			printf("Buffer type = %s\n",
+				(buf.getType() == TYPE_INVALID) ? "INVALID" :
+				buf.getType() == TYPE_GRAPHIC ? "GRAPHIC" : "LINEAR");
+			printf("buffer Class Destruct count %u\n", get_destruct_count());
+		}
+	};
+
+private:
+	static uint32_t m_destruct_count;
+	uint32_t m_len{0};
+	uint32_t m_size{0};
+	type_t m_type{TYPE_INVALID};
+};
+
+uint32_t Buffer::m_destruct_count = 0;
+uint32_t Buffer::get_destruct_count()
+{
+	// static member function can only access
+	// other static member variables
+	return m_destruct_count;
+}
+
+Buffer::Buffer(uint32_t len, uint32_t size, type_t Type)
+{
+	printf("Buffer Constructor\n");
+	m_len = len;
+	m_size = size;
+	m_type = Type;
+}
+
+Buffer::~Buffer()
+{
+	printf("Buffer Destructor\n");
+	m_destruct_count++;
+}
+
+uint32_t Buffer::getSize()
+{
+	return m_size;
+}
+
+uint32_t Buffer::getLength()
+{
+	return m_len;
+}
+
+Buffer::type_t Buffer::getType() const
+{
+	return m_type;
+}
+
+void Buffer::printBufInfo()
+{
+	printf("New buffer ==============\n");
+	printf("Buffer length = %u\n", this->getLength());
+	printf("Buffer size = %u\n", this->getSize());
+	printf("Buffer type = %s\n",
+		(this->getType() == TYPE_INVALID) ? "INVALID" :
+		this->getType() == TYPE_GRAPHIC ? "GRAPHIC" : "LINEAR");
+	printf("buffer Class Destruct count %u\n", get_destruct_count());
+
+}
+
+void printBufType(const Buffer *b)
+{
+	printf("New buffer ==============\n");
+
+// Not allowed - can only call other const functions
+//	printf("Buffer length = %u\n", b->getLength());
+//	printf("Buffer size = %u\n", b->getSize());
+
+// Call to const functions are allowed
+	printf("Buffer type = %s\n",
+		(b->getType() == Buffer::TYPE_INVALID) ? "INVALID" :
+		b->getType() == Buffer::TYPE_GRAPHIC ? "GRAPHIC" : "LINEAR");
+	printf("buffer Class Destruct count %u\n", Buffer::get_destruct_count());
+}
+
+void reset(Buffer *buf)
+{
+	// Friend function - can access private data of buf
+	buf->m_len = 0;
+	buf->m_size = 0;
+	buf->m_type = Buffer::TYPE_INVALID;
+}
+
+// =========================================
+// BufferFiller Class
+// =========================================
+
+class BufferFiller {
+public:
+	void fillBuffer(Buffer buf)
+	{
+		// friend class - can access private members
+		buf.m_len = 10;
+		buf.m_size = 10;
+	}
+};
+
+// =========================================
+// Tests
+// =========================================
+
+void test_class_with_ptr(Buffer *b)
+{
+	b->printBufInfo();
+}
+
+void test_class_with_ref(Buffer &b)
+{
+	b.printBufInfo();
+}
+
+void test_class()
+{
+
+	Buffer b;
+	b.printBufInfo();
+
+	test_class_with_ref(b);
+
+	Buffer c(10, 2, Buffer::TYPE_LINEAR);
+	c.printBufInfo();
+
+	Buffer *buf;
+	buf = new Buffer(100);
+	test_class_with_ptr(buf);
+	printBufType(buf);
+	reset(buf);
+	buf->printBufInfo();
+	delete buf;
+
+	// const obj
+	const Buffer cbuf(1, 2, Buffer::TYPE_GRAPHIC);
+	printBufType(buf);
+
+	// Anonymous Buffer object passed to friend class
+	BufferFiller bf;
+	bf.fillBuffer(Buffer());
+
+	// Nested class - BufferPrinter
+	Buffer::BufferPrinter bp;
+	bp.print(c);
+}
+
+int main()
+{
+	test_class();
+	return 0;
+}
+
